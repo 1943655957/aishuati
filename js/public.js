42086b36dbd34f58f098624199de0d0e558ec0d1
如此，也好 <1943655957@qq.com>
2024-08-17 11:42:33 +0800

    tmp

--- js/public.js
+++ js/public.js
@@ -1,11 +1,14 @@
 const JSONList = [
-    { id: "1", name: '学位英语语法专项', describe: '学位英语冠词语法', file: "syntax/article" },
-    { id: "2", name: '学位英语语法专项', describe: '学位英语代词语法', file: "syntax/pronoun" },
-    { id: "3", name: '学位英语语法专项', describe: '学位英语比较级/最高级语法', file: "syntax/more_max" },
-    { id: "4", name: '学位英语语法专项', describe: '学位英语介词语法', file: "syntax/preposition" },
-    { id: "5", name: '学位英语语法专项', describe: '学位英语连词语法', file: "syntax/conn" },
+    { id: "1", name: '学位英语语法专项', describe: '冠词语法', file: "syntax/article" },
+    { id: "2", name: '学位英语语法专项', describe: '代词语法', file: "syntax/pronoun" },
+    { id: "3", name: '学位英语语法专项', describe: '比较级/最高级语法', file: "syntax/more_max" },
+    { id: "4", name: '学位英语语法专项', describe: '介词语法', file: "syntax/preposition" },
+    { id: "5", name: '学位英语语法专项', describe: '连词语法', file: "syntax/conn" },
+    { id: "6", name: '学位英语语法专项', describe: '时态', file: "syntax/time" },
+    { id: "7", name: '学位英语语法专项', describe: '语态', file: "syntax/voice" },
+    { id: "8", name: '学位英语语法专项', describe: '情态动词', file: "syntax/modal_verb" },
     
-    { id: "201610", name: '00896 电子商务概论 2016年10月题库', describe: '00896 电子商务概论 201610真题试卷，包含选择和简答题，不包含分析和材料题', file: "2016 10.json" },
+    // { id: "201610", name: '00896 电子商务概论 2016年10月题库', describe: '00896 电子商务概论 201610真题试卷，包含选择和简答题，不包含分析和材料题', file: "2016 10.json" },
     // { id: "2017", name: '00896 电子商务概论 2017年题库 含解析', describe: '00896 电子商务概论 201704、201710真题试卷，包含选择和简答题，不包含分析和材料题', file: "2017.json" },
     // { id: "2018", name: '00896 电子商务概论 2018年题库', describe: '00896 电子商务概论 201804、201810真题试卷，包含选择和简答题，不包含分析和材料题', file: "2018.json" }
 ]
